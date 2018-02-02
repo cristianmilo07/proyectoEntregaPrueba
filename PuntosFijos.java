ecea1f442bb89f9e718aeec3d2614e86962f5c70
CRISTIAN\CRISTIAN CAMILO <cristianmilo07@gmail.com>
2018-02-01 22:45:00 -0500

    Se subé en el repositorio  el enunciado del ejercicio “pantalla LCD”

---
+++ PuntosFijos.java
@@ -0,0 +1,39 @@
+public class PuntosFijos {
+    
+    // Puntos fijos
+    private final int[] pf1;
+    private final int[] pf2;
+    private final int[] pf3;
+    private final int[] pf4;
+    private final int[] pf5;
+    
+    public PuntosFijos(){
+        // Inicializa variables
+        pf1 = new int[2];
+        pf2 = new int[2];
+        pf3 = new int[2];
+        pf4 = new int[2];
+        pf5 = new int[2];
+    }
+
+    public int[] getPf1() {
+        return pf1;
+    }
+
+    public int[] getPf2() {
+        return pf2;
+    }
+
+    public int[] getPf3() {
+        return pf3;
+    }
+
+    public int[] getPf4() {
+        return pf4;
+    }
+
+    public int[] getPf5() {
+        return pf5;
+    }
+    
+}
